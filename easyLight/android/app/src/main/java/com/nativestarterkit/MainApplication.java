da94daf39ab192f412c65f5f97b81b9ff53228c4
Jonathan Blanco <zlatanjon@hotmail.com>
2017-07-25 17:51:34 -0500

    add states an mun of API, add library photo, modified selected by addContracts

--- easyLight/android/app/src/main/java/com/nativestarterkit/MainApplication.java
+++ easyLight/android/app/src/main/java/com/nativestarterkit/MainApplication.java
@@ -3,7 +3,6 @@ package com.nativestarterkit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -15,12 +14,6 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-    @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -29,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
+          new MainReactPackage()
       );
     }
   };
--- easyLight/js/reducers/index.js
+++ easyLight/js/reducers/index.js
@@ -4,12 +4,17 @@ import drawer from "./drawer";
 import user from "./user";
 import list from "./list";
 import list_contracts from "./list_contracts";
+import list_states_mx from "./list_states_mx";
+import list_mun_mx from "./list_mun_mx";
+
 
 export default combineReducers({
   form: formReducer,
   drawer,
   user,
   list,
-  list_contracts
+  list_contracts,
+  list_states_mx,
+  list_mun_mx,
 
 });
