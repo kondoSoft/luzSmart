0ad4a3fca02c74551d5fcdcff53731b048f86db7
Luis Juarez <skaflon_war@hotmail.com>
2017-07-26 18:06:24 -0500

    fixed scrollview to form signin

--- easyLight/js/components/signin/index.js
+++ easyLight/js/components/signin/index.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import  { Text, Container, Content, Icon, Thumbnail, Button, Form, Item, Label, Input } from 'native-base';
-import { Platform, ScrollView, Dimensions } from 'react-native';
+import { Platform, ScrollView, Dimensions, Keyboard } from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import Header from '../header/index';
 import styles from './styles';
 
 const Screen = Dimensions.get('window');
 
-
 class SignIn extends Component {
   static navigationOptions = {
     header: null
   };
+  componentWillMount () {
+   this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow);
+   this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
+  }
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+  _keyboardDidShow () {
+
+  }
+  _keyboardDidHide () {
+
+  }
   render(){
-    console.log('this is the height of the movile',Screen.height)
+    console.log(Screen.height);
     return(
         <Container style={{height:Screen.height}}>
-          <Header zIndex navigation={this.props.navigation} title="NUEVO REGISTRO"/>
+          <Header zIndex navigation={this.props.navigation} title="Nuevo Registro"/>
           <ScrollView
             style={{height: Screen.height - 40}}
             ref='scroll'
             >
             <Grid style={{flex: 1}}>
-              <Row style={[styles.row__top,{height: '15%',paddingTop: 5, paddingBottom: 5}]}>
+              <Row style={styles.row__top}>
                 <Col style={styles.row__top__col__left}>
                   <Thumbnail source={ require('../../../images/profile.png')} />
                 </Col>
@@ -32,43 +45,61 @@ class SignIn extends Component {
                   {/* </Button> */}
                 </Col>
               </Row>
-              <Col style={{height: '75%'}}>
+              <Col>
                 <Form>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Nombres:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 80 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Nombres:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 80 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Apellidos:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 80 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Apellidos:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 80 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Email:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 80 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Email:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 80 : 80 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Contraseña:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 90 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Contraseña:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 90 : 90 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Confirmar contraseña:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 100 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Confirmar contraseña:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 100 : 100 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: 3, paddingBottom: 3}}>
-                    <Label>F. nacimiento:</Label>
-                    <Input onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 160 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>F. nacimiento:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 160 : 160 })} />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>Celular:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 200 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>Celular:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={()=>this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 200 : 200 })}
+                    />
                   </Item>
-                  <Item inlineLabel last style={{ alignItems: 'center',justifyContent: 'center',paddingTop: (Screen.height > 592)? 10  : 5, paddingBottom: (Screen.height > 552)? 10  : 5}}>
-                    <Label>C.P.:</Label>
-                    <Input style={{ height: '80%',padding: 0,margin: 0,paddingBottom: 3}} onFocus={() => this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 0 : 300 })} />
+                  <Item inlineLabel last style={styles.form__item}>
+                    <Label style={styles.text}>C.P.:</Label>
+                    <Input
+                      style={styles.form__item__input}
+                      onFocus={()=>this.refs['scroll'].scrollTo({y: (Platform.OS === 'ios')? 250 : 300 })}
+                    />
                   </Item>
                 </Form>
               </Col>
-              <Row style={[styles.row__botttom,{ height:'10%',paddingTop:10,paddingBottom:10}]}>
+              <Row style={styles.row__botttom}>
                 <Button
                   primary
                   style={styles.row__botttom__btn}
