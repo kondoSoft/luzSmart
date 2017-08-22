0fe6f777379c35849e9b1a1b575ebf8235bf5d41
Jonathan BLanco <zlatanjon@hotmail.com>
2017-08-22 15:06:08 -0500

    commit for pull

--- easyLight/js/actions/list_states_mx.js
+++ easyLight/js/actions/list_states_mx.js
@@ -10,7 +10,7 @@ export const GET_CONTRACT = 'GET_CONTRACT'
 export const SUCCES_CONTRACT = 'SUCCES_CONTRACT'
 
 
-const endPoint = 'http://192.168.1.64:8080';
+const endPoint = 'http://138.68.49.119:8080/';
 
 
 export function printStates(list):Action{
--- easyLight/js/actions/user.js
+++ easyLight/js/actions/user.js
@@ -1,7 +1,7 @@
 
 import type { Action } from './types';
 
-const endPoint = 'http://192.168.1.64:8080';
+const endPoint = 'http://138.68.49.119:8080/';
 
 export const SET_USER = 'SET_USER';
 export const LOGOUT = 'LOGOUT';
