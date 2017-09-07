0ec4542918162d63625be5826ba3732dab664663
Williams Rincon <wwallass.0310@gmail.com>
2017-09-07 11:32:48 -0500

    add Graphics

--- easyLight/android/app/src/main/java/com/easylight/MainApplication.java
+++ easyLight/android/app/src/main/java/com/easylight/MainApplication.java
@@ -3,6 +3,7 @@ package com.easylight;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.microsoft.codepush.react.CodePush;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new LinearGradientPackage(),
             new ImagePickerPackage(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
