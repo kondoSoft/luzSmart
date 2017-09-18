cb0415d66885ea54b766643d5bf005c44603a10d
Luis Juarez <skaflon_war@hotmail.com>
2017-09-18 12:56:25 -0500

    change validation login, email and password

--- easyLight/js/actions/list_states_mx.js
+++ easyLight/js/actions/list_states_mx.js
@@ -12,7 +12,7 @@ export const SUCCES_CONTRACT = 'SUCCES_CONTRACT'
 
 
 // const endPoint = 'http://138.68.49.119:8080';
-const endPoint = 'http://127.0.0.1:8080';
+const endPoint = 'http://127.0.0.1:8000';
 
 
 export function printStates(list):Action{
