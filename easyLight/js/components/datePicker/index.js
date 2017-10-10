7a0f26fca8cb9b2114ca73abec920ea2d00a63f4
Luis Juarez <skaflon_war@hotmail.com>
2017-10-10 16:57:52 -0500

    add helpers functions for datepicker and get day kwh

--- easyLight/js/components/datePicker/index.js
+++ easyLight/js/components/datePicker/index.js
@@ -1,46 +1,57 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-native-datepicker'
-
+import {
+  getMinimunYears
+} from '../../helpers'
 
 var date = new Date()
-var formatDate = date.getFullYear()+ '-' + (date.getMonth() + 1) + '-' + date.getDate()
+var formatDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
 
 export default class PickerDate extends Component {
-  constructor(props){
-
+  constructor (props) {
     super(props)
     this.state = {date: formatDate}
     this.setDate = this.setDate.bind(this)
+    this.date
+    this.maxDate
   }
-  setDate(date){
+  setDate (date) {
     this.setState({
-      date: date
+      date
     })
     this.props.func(date)
   }
-
-  render(){
+  componentWillMount () {
+    let date = new Date()
+    let year = date.getFullYear()
+    this.date = getMinimunYears(year)
+    this.maxDate = new Date(this.date, date.getMonth(), date.getDate())
+    this.setState({
+      date: this.maxDate
+    })
+  }
+  render () {
     return (
       <DatePicker
         style={{width: '57%'}}
         date={this.state.date}
-        mode="date"
+        mode='date'
         showIcon={false}
-        placeholder="select date"
-        format="YYYY-MM-DD"
+        placeholder='select date'
+        format='YYYY-MM-DD'
         // minDate={formatDate}
-        maxDate={formatDate}
-        confirmBtnText="Confirmar"
-        cancelBtnText="Cancelar"
+        maxDate={'1999-12-31'}
+        confirmBtnText='Confirmar'
+        cancelBtnText='Cancelar'
         customStyles={{
           dateInput: {
             height: 40,
             alignItems: 'flex-start',
             paddingLeft: 90,
             width: '20%',
-            borderWidth:0,
+            borderWidth: 0
           },
-          dateText:{
+          dateText: {
             fontSize: 16
           }
         }}
--- easyLight/js/helpers/index.js
+++ easyLight/js/helpers/index.js
@@ -1,16 +1,14 @@
 const getWeekday = (date) => {
-  date = new Date(date);
+  date = new Date(date)
   days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
   date = days[date.getDay()]
   return date
 }
 
-
-
 const getIVA = total => {
   if (total) {
     if (typeof total === 'string') {
-      const iva = 1.16;
+      const iva = 1.16
       try {
         total = parseInt(total)
       } catch (e) {
@@ -18,49 +16,67 @@ const getIVA = total => {
           return undefined
         }
       }
-      return total * iva;
-    }else {
-      const iva = 1.16;
-      return total * iva;
+      return total * iva
+    } else {
+      const iva = 1.16
+      return total * iva
     }
-  }else {
+  } else {
     return total
   }
-
 }
-
 
 const whileCosts = (kilowatt, countKwh) => {
     // console.log(kilowatt, countKwh)
-    var consumoTotal = 0;
-    if(kilowatt){
-      kilowatt = kilowatt.filter((item)=> {return (item.kilowatt>0)}).reverse()
+  var consumoTotal = 0
+  if (kilowatt) {
+    kilowatt = kilowatt.filter((item) => { return (item.kilowatt > 0) }).reverse()
 
-      while(countKwh >= 0 && kilowatt.length > 0) {
-        let range = kilowatt.pop()
-        if (countKwh > range.kilowatt){
-          let consumo = countKwh - range.kilowatt
-          countKwh -= range.kilowatt
-          consumo = range.kilowatt * range.cost
-          consumoTotal += consumo
+    while (countKwh >= 0 && kilowatt.length > 0) {
+      let range = kilowatt.pop()
+      if (countKwh > range.kilowatt) {
+        let consumo = countKwh - range.kilowatt
+        countKwh -= range.kilowatt
+        consumo = range.kilowatt * range.cost
+        consumoTotal += consumo
+      }
+
+      while (kilowatt.length == 0 && countKwh > 0) {
+        consumo = countKwh * range.cost
+        consumoTotal += consumo
+        countKwh -= range.kilowatt
+
+        if (countKwh < range.kilowatt) {
+          countKwh = 0
         }
-
-        while ( kilowatt.length == 0 && countKwh > 0){
-          consumo = countKwh * range.cost
-          consumoTotal += consumo
-          countKwh -= range.kilowatt
-
-          if (countKwh < range.kilowatt){
-            countKwh = 0
-          }
-          return consumoTotal
-        }
+        return consumoTotal
       }
     }
   }
-
+}
+const getDayInDates = (fechaMinima, fechaMaxima) => {
+  let fechaMin = Date.parse(fechaMinima)
+  let fechaMax = Date.parse(fechaMaxima)
+  let time = fechaMax - fechaMin
+  let daysInMiliSeconds = time / 1000
+  let daysInSeconds = daysInMiliSeconds / 60
+  let daysInMinutes = daysInSeconds / 60
+  let daysInHours = daysInMinutes / 24
+  return daysInHours
+}
+const getKwHrsTransCurrid = (kwInicial, kwFinal, translatedDays) => {
+  let diffKw = kwFinal - kwInicial
+  let perDays = diffKw / translatedDays
+  return perDays
+}
+const getMinimunYears = year => {
+  return year - 18
+}
 export {
   getIVA,
   whileCosts,
   getWeekday,
+  getDayInDates,
+  getKwHrsTransCurrid,
+  getMinimunYears
 }
